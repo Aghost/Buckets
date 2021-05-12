a10fe79b3970a906d14ae425eb43b6937609c6f6
Your Name <you@example.com>
2021-05-13 01:56:50 +0200

    cleanup Program.cs

--- Buckets/Program.cs
+++ Buckets/Program.cs
@@ -10,26 +10,17 @@ namespace Buckets
         static void Main(string[] args)
         {
             Bucket bucket = Bucket.CreateDefault(12);
-            bucket.GetStats();
             bucket.Fill(18);
-            bucket.GetStats();
             bucket.Empty();
-            bucket.GetStats();
 
             RainBarrel rainbarrel = RainBarrel.CreateDefault();
-            rainbarrel.GetStats();
             rainbarrel.Fill(101);
-            rainbarrel.GetStats();
 
             RainBarrel rainbarrel2 = RainBarrel.CreateDefault(2);
-            rainbarrel2.GetStats();
             rainbarrel2.Fill(33);
-            rainbarrel2.GetStats();
 
             OilBarrel oilbarrel = OilBarrel.CreateDefault(20);
-            oilbarrel.GetStats();
             oilbarrel.Fill(33);
-            oilbarrel.GetStats();
 
             oilbarrel.Fill(32, bucket);
         }
